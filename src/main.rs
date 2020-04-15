87eeb304155ac84640b548b99ec03aea8194fc61
Tatsu <mail@alexanderphillips.net>
2020-04-15 02:21:47 -0700

    Need to fix the hash to String conversion.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate lapp;
 extern crate md5;
+extern crate reqwest;
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 use std::fs;
 use std::io::SeekFrom;
+use std::string::ToString;
 
 fn main() {
 
@@ -21,20 +23,20 @@ Downloads the subtitles for supplied video.
     let language = args.get_string("language");
     let file = args.get_string("file");
     
-    hash_brown(file); // Get the hash of the video.
+    let hash = hash_brown(file); // Get the hash of the video.
     
 }
 
-fn hash_brown(file: String) -> io::Result<()> {
+fn hash_brown(file: String) -> md5::Digest {
 
-    let metadata = fs::metadata(&file)?;
-    let mut video_file = File::open(file)?;
+    let metadata = fs::metadata(&file).unwrap();
+    let mut video_file = File::open(file).unwrap();
     let mut first_buffer = vec![0; 65536]; // 64 x 1024 for 64kb.
     let mut second_buffer = vec![0; 65536]; // 64 x 1024 for 64kb.
     
-    video_file.read(&mut first_buffer)?;
-    video_file.seek(SeekFrom::Start(metadata.len() - 65536))?; // Sets to the last 64kb.
-    video_file.read(&mut second_buffer)?;
+    video_file.read(&mut first_buffer).unwrap();
+    video_file.seek(SeekFrom::Start(metadata.len() - 65536)).unwrap(); // Sets to the last 64kb.
+    video_file.read(&mut second_buffer).unwrap();
 
     first_buffer.extend(&mut second_buffer.iter().cloned()); // Not very efficient.
     
@@ -42,12 +44,26 @@ fn hash_brown(file: String) -> io::Result<()> {
 
     println!("Computed hash: {:x}", digest);
 
-    Ok(())
-
+    digest
 }
 
-//fn check_subdb(hash: ) -> () { 
+fn check_subdb(hash: md5::Digest) -> () { 
 
-   //let user_agent = "SubDB/1.0 (suboptimal/0.1; https://github.com/Tatsuonline/suboptimal.git)"
+    let hash_string = hash.to_string(); // TODO: Fix the string coversion.
+    let uri_builder = format!("http://sandbox.thesubdb.com/?action=download&hash={}&language=pt,en", hash_string); // For testing.
 
-//}
+    println!("URI: {}", uri_builder);
+
+    send_request(uri_builder);
+}   
+    
+
+fn send_request(uri: String) -> io::Result<()> {
+
+    let client = reqwest::Client::new();
+    let res = client.get(&uri)
+	.header("User-Agent", "SubDB/1.0 (suboptimal/0.1; https://github.com/Tatsuonline/suboptimal.git)")
+	.send();
+
+    Ok(())
+}
